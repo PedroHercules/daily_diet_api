1f4ec6213dc8ae98fe975760d6b2eac5581c5fe8
PedroHercules <pedroherculesdantas@gmail.com>
2023-05-14 19:27:47 -0300

    feat(meal-routes): return all user meals

--- src/routes/meal.ts
+++ src/routes/meal.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { createMealSchema } from '../schemas/mealSchemas'
 import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
+import { AuthUserType } from '../@types/user'
 
 export async function mealRoutes(app: FastifyInstance) {
   app.post(
@@ -11,11 +12,13 @@ export async function mealRoutes(app: FastifyInstance) {
     },
     async (request, reply) => {
       try {
+        const user = request.user as AuthUserType
         const mealBody = createMealSchema.parse(request.body)
 
         const meal = await knex('meals')
           .insert({
             id: randomUUID(),
+            user_id: user.id,
             ...mealBody,
           })
           .returning([
@@ -29,6 +32,26 @@ export async function mealRoutes(app: FastifyInstance) {
           ])
 
         return reply.status(201).send(meal[0])
+      } catch (error: any) {
+        return reply.status(error.status || 500).send({
+          message: error.message,
+        })
+      }
+    }
+  )
+
+  app.get(
+    '/',
+    {
+      onRequest: [app.authenticate],
+    },
+    async (request, reply) => {
+      try {
+        const user = request.user as AuthUserType
+
+        const meals = await knex('meals').where('user_id', user.id)
+
+        return reply.status(200).send(meals)
       } catch (error: any) {
         return reply.status(error.status || 500).send({
           message: error.message,
