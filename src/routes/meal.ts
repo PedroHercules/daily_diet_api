4312051a0db860dc131407d9944e2dc883c18544
PedroHercules <pedroherculesdantas@gmail.com>
2023-05-14 21:26:51 -0300

    feat(meal-route): get meal by id

--- src/routes/meal.ts
+++ src/routes/meal.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from 'fastify'
+import { FastifyInstance, FastifyRequest } from 'fastify'
 import { createMealSchema } from '../schemas/mealSchemas'
 import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
@@ -52,6 +52,36 @@ export async function mealRoutes(app: FastifyInstance) {
         const meals = await knex('meals').where('user_id', user.id)
 
         return reply.status(200).send(meals)
+      } catch (error: any) {
+        return reply.status(error.status || 500).send({
+          message: error.message,
+        })
+      }
+    }
+  )
+
+  app.get(
+    '/:id',
+    {
+      onRequest: [app.authenticate],
+    },
+    async (request, reply) => {
+      try {
+        const { id } = request.params as { id: string }
+        const user = request.user as AuthUserType
+
+        const meal = await knex('meals').where('id', id).first()
+        if (meal.user_id !== user.id) {
+          return reply.status(401).send({
+            message: 'This user is not authorized to access this meal!',
+          })
+        }
+        if (!meal) {
+          return reply.status(404).send({
+            message: 'Meal not found',
+          })
+        }
+        return reply.status(200).send(meal)
       } catch (error: any) {
         return reply.status(error.status || 500).send({
           message: error.message,
